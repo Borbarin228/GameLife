6324327ec7e1514e8f7094edd4dcaf96689cb3be
Aboldosus <114693651+Borbarin228@users.noreply.github.com>
2024-05-10 10:36:43 +0500

    Большая обнова

---
+++ header.php
@@ -0,0 +1,13 @@
+<header class="head">
+
+    <div class="panel">
+
+        <div class = "play"> <a href="">ИГРАТЬ</a> </div>
+        <div class = "history"> <a href="">ИСТОРИЯ</a> </div>
+        <div class = "application"> <a href="">ПРИМЕНЕНИЕ</a> </div>
+        <div class = "rules"> <a href="">ПРАВИЛА</a> </div>
+        <div class = "contacts"> <a href="">КОНТАКТЫ</a> </div>
+
+    </div>
+
+</header>
